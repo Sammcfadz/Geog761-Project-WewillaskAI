08be805c1a0b8bdf02d15ca1c3de366bad801970
Sammcfadz <sammcfadz@gmail.com>
2025-09-23 11:40:52 +1200

    Created Basic Bounding Region For Testing Further Functions

--- create_auckland_data.py
+++ create_auckland_data.py
@@ -1,4 +1,6 @@
 def import_test_region():
+    # Note this should be changed to the new define region file when that is completed
+    # For the moment, its just a square in muriwai for testing purposes
     from define_region import ___
 
 def create_tiles_of_auckland():
