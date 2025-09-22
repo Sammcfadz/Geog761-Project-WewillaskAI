8d3cfca3728245582c0b225838f0224323adc4d0
Sammcfadz <sammcfadz@gmail.com>
2025-09-23 11:44:32 +1200

    Create Test File for Map

--- create_auckland_data.py
+++ create_auckland_data.py
@@ -1,7 +1,7 @@
 def import_test_region():
     # Note this should be changed to the new define region file when that is completed
     # For the moment, its just a square in muriwai for testing purposes
-    from define_region import ___
+    from define_region_simplistic import create_simplistic_region
 
 def create_tiles_of_auckland():
     pass
